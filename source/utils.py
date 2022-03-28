9ff109d0d0235e498e689be16da382c5891c2380
Kareem <kareemmahlees@gmail.com>
2022-03-28 21:19:40 +0200

    fixed some bugs in parse for creating function

--- source/utils.py
+++ source/utils.py
@@ -1,29 +1,38 @@
-from typing import Any, Callable, Dict, List, Tuple
+from typing import Any, Callable, Dict, List, Optional, Tuple
 import json
 
-import _constants
+import constants
 
-Vector = List[Tuple[str, str, Any]]
+Vector = List[Tuple[str, str, Optional[Any]]]
 
 
-def parse_data_for_creating(data: Vector) -> Dict:
+def parse_for_pg_creating(db_id, data: Vector, icon=None, cover_url=None) -> Dict:
     """
     function for collecting the inputed params into a notion acceptable dict for further conversion into json
-    Args:
+    :param data:
         list of tuples each containing (property name , property type , value of property )
     """
-    parsed_dict = {}
-    for name, type_, content in data:
-        if type_.upper() not in {
-            i for i in _constants.__dict__.keys()
-        }:  # check if property type exists
-            raise ValueError(f"Property {type_} Doesn't exist")
+    data_dict = {}
+    temp_dict = {}
+    if icon is not None:
+        temp_dict.update(constants.ICON(icon))
+    if cover_url is not None:
+        temp_dict.update(constants.COVER(cover_url))
+    if data is not None:
+        for name, type_, content in data:
+            if type_.upper() not in {
+                i for i in constants.__dict__.keys()
+            }:  # check if property type exists
+                raise ValueError(f"Property {type_} Doesn't exist")
 
-        else:
-            type_attr = getattr(_constants, type_.upper())
-            parsed_dict.update(type_attr(name, content))
-
-    return parsed_dict
+            else:
+                type_attr = getattr(constants, type_.upper())
+                data_dict.update(type_attr(name, content))
+        parsed_dict = constants.CREATING_PAGE_TEMPLATE(db_id, data_dict)
+        parsed_dict.update(temp_dict)
+        return parsed_dict
+    else:
+        return
 
 
 def parse_into_json(parsed_data: Callable[[Vector], Dict]):
