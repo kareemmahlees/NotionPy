632f19f3fd3a5e2de5bf2b3e5ea074dddfbfde88
Kareem <kareemmahlees@gmail.com>
2022-04-01 21:12:31 +0200

    added the query feature

--- source/notionpy.py
+++ source/notionpy.py
@@ -1,27 +1,52 @@
-import json
-import requests
+from typing import List, Tuple, Optional, Any
+
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+from source.create import Create
+from source.query import Query
+
+# Vector1 = List[Tuple[str, str, Optional[Any]]]
+# Vector2 = List[Tuple[str, str]]
 
 
-class NotionClient(requests):
-    def __init__(self, token: str, db_id: str = None, page_id: str = None):
-        self.db_id = db_id
-        self.token = token
-        self.headers = {
-            "Authorization": "Bearer " + self.token,
-            "Notion-Version": "2022-02-22",
-            "Content-Type": "application/json",
-        }
+class NotionClient(Create, Query):
+    def __init__(self, token: str) -> None:
+        Create.TOKEN = token
+        Query.TOKEN = token
 
-    def create(self):
-        def page(self):
-            pass
+    class create:
+        def page(
+            database_id: str,
+            data: List[Tuple[str, str, Optional[Any]]],
+            icon: Optional[str] = None,
+            cover: Optional[str] = None,
+        ) -> None:
+            Create.page(Create, database_id, icon, cover, data)
 
-        def db(self):
-            pass
+        def db(
+            page_id: str,
+            title: str,
+            data: List[Tuple[str, str]],
+            icon: Optional[str] = None,
+            cover: Optional[str] = None,
+        ) -> None:
+            Create.db(Create, page_id, title, icon, cover, data)
 
-    def retreive(self):
-        def page(self):
-            pass
+    class query:
+        def page(
+            page_id,
+            in_json: Optional[bool] = False,
+            json_indent: Optional[int] = None,
+            print_data: Optional[bool] = False,
+        ) -> None:
+            Query.page(Query, page_id, in_json, json_indent, print_data)
 
-        def db(self):
-            pass
+        def db(
+            db_id,
+            in_json: Optional[bool] = False,
+            json_indent: Optional[int] = None,
+            print_data: Optional[bool] = False,
+        ) -> None:
+            Query.db(Query, db_id, in_json, json_indent, print_data)
