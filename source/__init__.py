1fe9485436e298623d0c51769629656178b62d2e
Kareem <kareemmahlees@gmail.com>
2022-04-05 04:20:50 +0200

    added base init

---
+++ source/__init__.py
@@ -0,0 +1,30 @@
+#        ________________________
+#        |\                      \
+#        | \ _____________________\
+#        |  |                      |
+#        |  |       |\     |       |
+#        |  |       | \    |       |
+#        |  |       |  \   |       |
+#        |  |       |   \  |       |
+#        \  |       |    \ |       |
+#         \ |       |     \|       |
+#          \|______________________|
+
+
+"""
+################# notionpy library #################### 
+
+-> notionpy is a python library made for simplifying interaction with notion for developers 
+to be able to integrate notion with your programme easily 
+
+-> Basic usage :
+        >>> Creating page \ database
+                NotionClient.create.page\db
+        >>> Querying page \ database
+                NotionClient.query.page\db
+        >>> Updating page 
+                NotionClient.update.page
+
+::copyright: (c) 2022 by Kareem Ebrahim.:
+
+"""
