ed83689a6aae61d0d14867474d446f669e744f4c
Kareem <kareemmahlees@gmail.com>
2022-04-05 04:26:35 +0200

    added update class

---
+++ source/update.py
@@ -0,0 +1,56 @@
+from typing import List, Tuple, Optional, Any
+
+import requests
+
+from source import constants
+from source.utils import parse_for_pg_creation, parse_into_json, parse_for_updating_db
+
+Vector = List[Tuple[str, str, Optional[Any]]]
+
+
+class Update:
+    TOKEN = None
+
+    def __init__(self) -> None:
+        pass
+
+    def page(
+        self,
+        pg_id: str,
+        icon: Optional[str] = None,
+        cover_url: Optional[str] = None,
+        data: Optional[Vector] = None,
+        archived: Optional[bool] = False,
+    ) -> None:
+        data_ = parse_for_pg_creation(
+            db_id=pg_id, data=data, icon=icon, cover_url=cover_url
+        )  # the pg_id and db_id params are little bit confusing to understand but it works just fine
+        data_.update(constants.ARCHIVE(archived))
+        data_ = parse_into_json(data_)
+        requests.patch(
+            url=constants.UPDATE_PAGE_URL(pg_id),
+            headers=constants.HEADERS(Update.TOKEN),
+            data=data_,
+        )
+
+    # not currently in use for some bugs but left for feature updates
+    def db(
+        self,
+        db_id: str,
+        title: str,
+        icon: Optional[str] = None,
+        cover_url: Optional[str] = None,
+        data: Vector = None,
+        archived: Optional[bool] = False,
+    ) -> None:
+        data_ = parse_for_updating_db(
+            db_id=db_id, data=data, title=title, icon=icon, cover_url=cover_url
+        )  # the pg_id and db_id params are little bit confusing to understand but it works just fine
+        data_.update(constants.ARCHIVE(archived))
+        data_ = parse_into_json(data_)
+        res = requests.patch(
+            url=constants.UPDATE_PAGE_URL(db_id),
+            headers=constants.HEADERS(Update.TOKEN),
+            data=data_,
+        )
+        print(res.json())
