bf09bc4a3912be50cdce3f756eb2e4f526e3c8bb
Kareem <kareemmahlees@gmail.com>
2022-04-20 01:01:31 +0200

    added sorting and querying features

--- NotionPy/query.py
+++ NotionPy/query.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional, Tuple
 
 import requests
 
@@ -42,10 +42,32 @@ class Query:
         in_json: Optional[bool] = False,
         json_indent: Optional[int] = None,
         print_data: Optional[bool] = False,
+        sort: Optional[List[Tuple]] = None,  # [(prop_name,ascending or descending)]
+        Filter: Optional[List[Tuple]] = None,  # [(prop_name,prop_type,condition,value)]
+        and_filter: Optional[
+            List[Tuple]
+        ] = None,  # [(prop_name,prop_type,condition,value)]
+        or_filter: Optional[
+            List[Tuple]
+        ] = None,  # [(prop_name,prop_type,condition,value)]
     ):
+        # the conditions is the same as in Notion Gui
+        queyring_data = {}
+        if sort is not None:
+            queyring_data.update(constants.SORT(sorting_info=sort))
+        if Filter is not None:
+            queyring_data.update(constants.FILTER(filtering_info=Filter))
+        if and_filter is not None:
+            queyring_data.update(constants.AND_FILTER(filtering_info=and_filter))
+        if or_filter is not None:
+            queyring_data.update(constants.OR_FILTER(filtering_info=or_filter))
+
         res = requests.post(
-            url=constants.QUERY_DB_URL(db_id), headers=constants.HEADERS(Query.TOKEN)
+            url=constants.QUERY_DB_URL(db_id),
+            data=parse_into_json(queyring_data),
+            headers=constants.HEADERS(Query.TOKEN),
         )
+
         data = (
             parse_into_json(res.json(), json_indent)
             if in_json is True
